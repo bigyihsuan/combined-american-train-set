d2c8ed16d3572223c5de3a831698abae58a89d93
bigyihsuan <bigyihsuan@gmail.com>
2024-10-24 14:33:01 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-NGRF_DIR=/mnt/c/Users/bigyi/OneDrive/Documents/OpenTTD/newgrf
+# NGRF_DIR=/mnt/c/Users/bigyi/OneDrive/Documents/OpenTTD/newgrf
+NGRF_DIRS=(/mnt/c/Users/bigyi/Documents/OpenTTD/newgrf /mnt/c/Users/bigyi/OneDrive/Documents/OpenTTD/newgrf)
 USAGE="usage: ./build.sh (default | extract | compile | bundle | install | clean)"
 GRF_PATH=./dist/cats.grf
+
+function LINE() {
+	echo "--------"
+}
 
 function default() {
 	# extract
@@ -13,34 +18,48 @@ function default() {
 function extract() {
     echo "Extracting vehicle info..."
     python3 src/extractProps.py
+    echo "Extracting vehicle info complete."
+	LINE
 }
 
 function compile() {
     echo "Compiling GRF..."
     python3 src/cats-grfpy.py
+    echo "Compiling GRF complete."
+	LINE
 }
 
 function bundle() {
     echo "Bundling GRF..."
-	rm cats.tar
+	rm -v cats.tar
 	mkdir -p dist
-	cp README.md dist/readme.txt
-	cp LICENSE dist/license.txt
-	cp changelog.md dist/changelog.txt
+	cp -v README.md dist/readme.txt
+	cp -v LICENSE dist/license.txt
+	cp -v changelog.md dist/changelog.txt
 	tar cvf cats.tar dist
+	LINE
 }
 
 function install() {
     echo "Installing GRF..."
     if [[ -e $GRF_PATH ]]; then
-		cp $GRF_PATH $NGRF_DIR
-        echo "Successfully installed."
+		for dir in "${NGRF_DIRS[@]}"; do
+			cp -v $GRF_PATH $dir
+		done
+		echo "Successfully installed."
+	else
+		echo "GRF path '$GRF_PATH' does not exist."
 	fi
+	LINE
 }
 
 function clean() {
     echo "Cleaning installation dir..."
-	rm "$NGRF_DIR/cats.grf"
+	for dir in "${NGRF_DIRS[@]}"; do
+		rm -v "$dir/cats.grf"
+	done
+	echo "Cleaning complete."
+	LINE
 }
 
 if [[ "$#" -eq 0 ]]; then
